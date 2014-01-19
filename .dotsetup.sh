5a5a0dd28714f6c9d3742f24ae373a543e5e3e0a
Zachary Sloss <zachary.sloss@gmail.com>
2014-01-19 12:41:53 +0800

    First commit

---
+++ .dotsetup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+##################################################
+# Backup dotfiles, create symlinks to new dotfiles
+##################################################
+
+dir=~/.dotfiles
+olddir=~/.dotfiles_old
+files="bashrc vimrc"
+
+###################
+
+echo "Creating $olddir..."
+mkdir -p $olddir
+echo "Success"
+
+cd $dir
+
+for file in $files; do
+	echo "Backing up old $file..."
+	mv ~/.$file $olddir/
+	echo "Creating symlink to $file..."
+	ln -s $dir/$file ~/.$file
+done
